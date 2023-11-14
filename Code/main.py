a6d0a62b08294e1c2c49fbd7193079aff04e9732
rayhan-rafin <rayhan.rafin2@gmail.com>
2023-11-14 18:44:16 +0600

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -1,5 +1,5 @@
 import mysql.connector
-import easygui
+import easygui as eg
 from datetime import date
 
 con = mysql.connector.connect(host="localhost", user="root", passwd="", autocommit=True)
@@ -12,32 +12,32 @@ c.execute("create table if not exists librarian(user_name varchar(50) Not null,u
 
 
 def add_book():
-    # Define the field names
-    add_book_names = ["Book ID", "Book name", "Genre","Author"]
-
-   # Display the form
-    add_book_values = easygui.multenterbox("Enter Book information", "New book entry", add_book_names)
-
-    sql = 'insert into books(b_id,b_name,genre,author) values(%s,%s,%s,%s)'
-    c.execute(sql, add_book_values)
-
+    try:
+        add_book_names = ["Book ID", "Book name", "Genre","Author"]
+        add_book_values = eg.multenterbox("Enter Book information", "New book entry", add_book_names)
+        sql = 'insert into books(b_id,b_name,genre,author) values(%s,%s,%s,%s)'
+        c.execute(sql, add_book_values)
+    except:
+        l_menu()
 
 def delete_book():
-    delete_book_names = ["Book ID"]
-    delete_book_values = easygui.multenterbox("Enter Book information", "Delete Book", delete_book_names)
-    c.execute(f"delete from books where b_id= {delete_book_values[0]}")
-    display_books()
-
+    try:
+        delete_book_names = ["Book ID"]
+        delete_book_values = eg.multenterbox("Enter Book information", "Delete Book", delete_book_names)
+        c.execute(f"delete from books where b_id= {delete_book_values[0]}")
+        display_books()
+    except:
+        l_menu()
 
 def issue_book():
     sql = "SELECT * FROM `books` ORDER BY cast(b_id as int)"
     c.execute(sql)
     issue_book_names = ["Book id","Student Name","Student ID"]
-    issue_book_values = easygui.multenterbox("Enter Book information", "Issue Book", issue_book_names)
+    issue_book_values = eg.multenterbox("Enter Book information", "Issue Book", issue_book_names)
     c.execute(f"select b_id from books where b_id = '{issue_book_values[0]}' and available='YES'")
     res = c.fetchall()
     if len(res)==0:
-        easygui.msgbox("Book is not available")
+        eg.msgbox("Book is not available")
         return
     today = str(date.today())
     c.execute(f"insert into issue_details values('{issue_book_values[0]}','{issue_book_values[2]}','{issue_book_values[1]}','{today}')")
@@ -50,7 +50,7 @@ def print_librarian(data):
     for i in data:
         result+="|"+"%30s"%i[0]+' |'+"%25s"%i[1]+' |'+"\n"
     result += "===============================================================================\n"
-    easygui.msgbox(result)
+    eg.msgbox(result)
     admin()
 
 def show_librarian():
@@ -62,50 +62,51 @@ def show_librarian():
 def add_librarian():
     try:
            librarian_names = ["User name", "Password"]
-           librarian_values = easygui.multenterbox("Enter information", "Admin",  librarian_names)
+           librarian_values = eg.multenterbox("Enter information", "Admin",  librarian_names)
            sql = 'insert into librarian(user_name,user_pass) values(%s,%s)'
            c.execute(sql, librarian_values)
     except:
-        admin()    
+        l_menu()    
 
 def delete_librarian():
     try:
         delete_librarian_names = ["User name"]
-        delete_librarian_values = easygui.multenterbox("Enter librarian name", "Delete librarian", delete_librarian_names)
+        delete_librarian_values = eg.multenterbox("Enter librarian name", "Delete librarian", delete_librarian_names)
         c.execute(f"delete from librarian where user_name = '{delete_librarian_values[0]}'")
     except:
         admin()
 
 def admin():
-    admin_ch = easygui.buttonbox(""" Select an option """, choices=['Show all librarian','Add librarian', 'Delete librarian'])
+    admin_ch = eg.buttonbox(""" Select an option """, choices=['Show all librarian','Add librarian', 'Delete librarian','Exit'])
     if admin_ch == 'Show all librarian':
         show_librarian()
-    if admin_ch == 'Add librarian':
+    elif admin_ch == 'Add librarian':
         add_librarian()
-    if admin_ch == 'Delete librarian':
+    elif admin_ch == 'Delete librarian':
         delete_librarian()
-
+    else:
+        home()
 
 def return_book():
     return_book_names = ["Book ID"]
-    return_book_values = easygui.multenterbox("Enter information", "Return book", return_book_names)
+    return_book_values = eg.multenterbox("Enter information", "Return book", return_book_names)
     c.execute(f"select issue_details. *, books.b_name FROM issue_details INNER JOIN books on issue_details.b_id = books.b_id HAVING issue_details.b_id = '{return_book_values[0]}'")
     res = c.fetchall()
     if len(res)==0:
-        easygui.msgbox('Book is not issued')
+        eg.msgbox('Book is not issued')
         return
     res = res[0]
     days = (date.today()-res[3]).days
     print(days)
     if days>14:
         fine = (days-14)*20
-        payment = easygui.ynbox(f'{fine} BDT fine is due. Payment complete?')
+        payment = eg.ynbox(f'{fine} BDT fine is due. Payment complete?')
         if payment == False:
-            easygui.msgbox("Payment incomplete. Book was not returned.")
+            eg.msgbox("Payment incomplete. Book was not returned.")
             return
     c.execute("update books set available='yes' where b_id='" + return_book_values[0] + "'")
     c.execute("delete from issue_details where b_id = %s", (return_book_values[0],))
-    easygui.msgbox(f"'{res[4]}' book has been successfully returned by {res[1]}")
+    eg.msgbox(f"'{res[4]}' book has been successfully returned by {res[1]}")
 
 
 def print_books(data):
@@ -115,7 +116,7 @@ def print_books(data):
     for i in data:
         result+="|"+"%13s"%i[0]+' |'+"%14s"%i[1]+' |'+"%13s"%i[2]+' |'+"%15s"%i[3]+' |'+"%13s"%i[4]+" |"+"\n"
     result += "===============================================================================\n"
-    easygui.msgbox(result)
+    eg.msgbox(result)
 
 
 
@@ -126,7 +127,7 @@ def print_issuedbooks(data):
     for i in data:
         result+="|"+"%10s"%i[0]+' |'+"%15s"%i[4]+' |'+"%15s"%i[1]+' |'+"%15s"%i[2]+" |"+"%13s"%i[3]+" |"+"\n"
     result += "===============================================================================\n"
-    easygui.msgbox(result)
+    eg.msgbox(result)
 
 
 def display_books():
@@ -175,54 +176,41 @@ def modify_info():
 def a_menu():
     try:
         Admin_names = ["User Name", "Password"]
-        Admin_values = easygui.multenterbox("Enter your information", "Personal Information", Admin_names)
+        Admin_values = eg.multenterbox("Enter your information", "Personal Information", Admin_names)
         if Admin_values[0] == 'a' and Admin_values[1] == '123':
             admin()
         else:
-            easygui.msgbox('Wrong username or Password,try again')
+            eg.msgbox('Wrong username or Password,try again')
             home()
     except:
         home()
 
 
 def l_menu():
-        try:
-            field_names = ["User Name", "Password"]
-            field_values = easygui.multenterbox("Enter your information", "Personal Information", field_names)
-            sql = f"Select user_name,user_pass from librarian where user_name='{field_values[0]}' and user_pass='{field_values[1]}'"
-            c.execute(sql)
-            librarian_res = c.fetchall()
-            if len(librarian_res)!=0:
-                easygui.msgbox('Welcome Librarian')
-                while True:
-                    ch = easygui.buttonbox(""" Select an option """, choices=['Add book', 'Issue book','Display books','Return book','Delete book','Modify info' ,'Exit'])
-                    if ch == 'Add book':
-                        add_book()
-                    elif ch == 'Issue book':
-                        issue_book()
-                    elif ch == 'Return book':
-                        return_book()
-                    elif ch == 'Display books':
-                        choice = easygui.buttonbox("Select a choice", choices=['All books', 'Issued books', 'Particular book'])
-                        if choice == 'All books':
-                            display_books()
-                        elif choice == 'Issued books':
-                            display_issued_books()
-                        elif choice == 'Particular book':
-                            select_book()
-                        else:
-                            print('wrong choice')
-                    elif ch == 'Delete book':
-                        delete_book()
-                    elif ch == 'Modify info':
-                        modify_info()
-                    else:
-                        break
+    while True:
+        ch = eg.buttonbox(""" Select an option """, choices=['Add book', 'Issue book','Display books','Return book','Delete book','Modify info' ,'Exit'])
+        if ch == 'Add book':
+            add_book()
+        elif ch == 'Issue book':
+            issue_book()
+        elif ch == 'Return book':
+            return_book()
+        elif ch == 'Display books':
+            choice = eg.buttonbox("Select a choice", choices=['All books', 'Issued books', 'Particular book'])
+            if choice == 'All books':
+                display_books()
+            elif choice == 'Issued books':
+                display_issued_books()
+            elif choice == 'Particular book':
+                select_book()
             else:
-                easygui.msgbox('Wrong username or Password,try again')
-                home()
-        except:
-            home()
+                print('wrong choice')
+        elif ch == 'Delete book':
+            delete_book()
+        elif ch == 'Modify info':
+            modify_info()
+        else:
+            break
 
 def s_menu():
     try:
@@ -232,12 +220,26 @@ def s_menu():
 
 def home():
     # Display a box with choices
-    user_type = easygui.buttonbox("Select User type", choices=['Admin','Librarian', 'Student'])
+    user_type = eg.buttonbox("Select User type", choices=['Admin','Librarian', 'Student'])
 
     if user_type == "Admin":
         a_menu()
+
     if user_type == "Librarian":
-        l_menu()
+        try:
+            field_names = ["User Name", "Password"]
+            field_values = eg.multenterbox("Enter your information", "Personal Information", field_names)
+            sql = f"Select user_name,user_pass from librarian where user_name='{field_values[0]}' and user_pass='{field_values[1]}'"
+            c.execute(sql)
+            librarian_res = c.fetchall()
+            if len(librarian_res)!=0:
+                l_menu()
+            else:
+                eg.msgbox('Wrong username or Password,try again')
+                home()
+        except:
+            home()       
+
     if user_type == "Student":
         s_menu()
 home()
